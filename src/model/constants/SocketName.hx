3224a0989fc1478415acd43bbbdd23aa95143fc5
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-03-08 00:34:53 +0100

    new extra names

--- src/model/constants/SocketName.hx
+++ src/model/constants/SocketName.hx
@@ -4,6 +4,9 @@ class SocketName {
 	public static var SEND:String = "send";
 	public static var MESSAGE:String = "message";
 	public static var IMAGE:String = "image";
+	public static var SEQUENCE:String = "sequence";
+	public static var COMBINE:String = "combine";
+	public static var MARKDOWN:String = "md";
 	public static var RENDER_FRAME:String = "render-frame";
 	public static var TEST:String = "test";
 
